946d53238b8f4e8201e8cfd7200345a7a594c1eb
Travis CI User <travis@example.org>
2016-11-07 15:56:23 +0000

    publish: Webpack transformation
    generated from commit df9f45257493e3ffe3a12c30d6a4597509396677

--- app/components/comparison/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./components/comparison.module'));
-__export(require('./components/comparison.component'));
-
-//# sourceMappingURL=index.js.map
